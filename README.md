d55555aa39b0bf9776e4e92650f5e3d0d871fd2f
foxamanda2 <fox.amanda2@gmail.com>
2021-02-11 20:53:30 -0500

    Start Of JS

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Roshambo JS
+
+## PEDAC
+
+- Player 1
+- Find the rock button
+
+  - When the rock button is pushed change h3 to say rock (string)
+
+- Find the paper button
+
+  - When the paper button is pushed change h3 to say paper(string)
+
+- Find the scissors button
+
+  - When the scissors button is pushed change h3 to say scissors(string)
+
+- Find the name button
+
+  - Change the name to the name entered
+
+- Player 2
+- Find the rock button
+
+  - When the rock button is pushed change h3 to say rock(string)
+  -
+
+- Find the paper button
+
+  - When the paper button is pushed change h3 to say paper(string)
+
+- Find the scissors button
+
+  - When the scissors button is pushed change h3 to say scissors (string)
+
+- Find the name button
+
+  - fChange the name to the name entered
+
+- Comparison
+
+  - Compare .team1 h3 and .team2 h3
+
+    - Have it say there is a Draw!
+
+  - if (player1 says rock && player 2 says scissors)
+    - return "Player 1 Won"
+  - if(player1 says scissors && player 2 says rock)
+    - return "Player 1 Won"
+  - if (player1 says paper && player2 says rock)
+
+    - Return "Player 1 Won"
+
+  - else
+    - return "Player 2 Won"
--- public/main.js
+++ public/main.js
@@ -1,7 +1,77 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  // Rock Team 1
+  const rockTeam1 = document.querySelector('.firstPlayer fieldset i#one')
+  function addRock1() {
+    const thingClickedOn = document.querySelector('.firstPlayer h3')
+    thingClickedOn.textContent = 'Rock'
   }
+  rockTeam1.addEventListener('click', addRock1)
+  // Paper Team 1
+  const paperTeam1 = document.querySelector('.firstPlayer fieldset i#two')
+  function addPaper1() {
+    const thingClickedOn = document.querySelector('.firstPlayer h3')
+    thingClickedOn.textContent = 'Paper'
+  }
+  paperTeam1.addEventListener('click', addPaper1)
+
+  // Scissors Team 1
+  const scissorTeam1 = document.querySelector('.firstPlayer fieldset i#three')
+  function addScissor1() {
+    const thingClickedOn = document.querySelector('.firstPlayer h3')
+    thingClickedOn.textContent = 'Scissor'
+  }
+  scissorTeam1.addEventListener('click', addScissor1)
+
+  // Rock Team 2
+  const rockTeam2 = document.querySelector('.secondPlayer fieldset i#one')
+  function addRock2() {
+    const thingClickedOn = document.querySelector('.secondPlayer h3')
+    thingClickedOn.textContent = 'Rock'
+  }
+  rockTeam2.addEventListener('click', addRock2)
+  // Paper Team 2
+  const paperTeam2 = document.querySelector('.secondPlayer fieldset i#two')
+  function addPaper2() {
+    const thingClickedOn = document.querySelector('.secondPlayer h3')
+    thingClickedOn.textContent = 'Paper'
+  }
+  paperTeam2.addEventListener('click', addPaper2)
+
+  // Scissors Team 2
+  const scissorTeam2 = document.querySelector('.secondPlayer fieldset i#three')
+  function addScissor2() {
+    const thingClickedOn = document.querySelector('.secondPlayer h3')
+    thingClickedOn.textContent = 'Scissor'
+  }
+  scissorTeam2.addEventListener('click', addScissor2)
+
+  //  Comparisons
+  const firstPlayerChoice = document.querySelector('.firstPlayer h3')
+  const secondPlayerChoice = document.querySelector('.secondPlayer h3')
+
+  function winner() {
+    if (firstPlayerChoice === secondPlayerChoice) {
+      const draw = document.querySelector('section footer')
+      draw.textContent = 'ITS A DRAW'
+    }
+    if (firstPlayerChoice === 'Rock' && secondPlayerChoice === 'Scissors') {
+      const winner = document.querySelector('.firstPlayer footer')
+      winner.textContent = 'YOU WON!'
+    }
+    if (firstPlayerChoice === 'Scissor' && secondPlayerChoice === 'Rock') {
+      const winner = document.querySelector('.firstPlayer footer')
+      winner.textContent = 'YOU WON!'
+    }
+    if (firstPlayerChoice === 'Paper' && secondPlayerChoice === 'Rock') {
+      const winner = document.querySelector('.firstPlayer footer')
+      winner.textContent = 'YOU WON!'
+    } else {
+      const winner = document.querySelector('.secondPlayer footer')
+      winner.textContent = 'YOU WON!'
+    }
+  }
+  firstPlayerChoice.addEventListener('click', winner)
+  secondPlayerChoice.addEventListener('click', winner)
 }
 
 document.addEventListener('DOMContentLoaded', main)
